5b160041ad179de6af7b42199b33546ef22facaf
risson <risson.yao@gmail.com>
2013-05-31 17:42:08 +0800

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,21 +1,17 @@
-function onClickHandler() {
-  return function() {
-  var a = document.getElementById('a');
-	a.onclick = alert(a.getAttribute('href'));
-}
-};
+function getClickHandler() {
+  return function(info, tab) {
 
+    // The srcUrl property is only available for image elements.
+    var url = 'info.html#' + info.srcUrl;
+
+    // Create a new window to the info page.
+    chrome.windows.create({ url: url, width: 520, height: 660 });
+  };
+};
 
 chrome.contextMenus.create({
 	  "title" : "Add link info",
 	  "type" : "normal",
 	  "contexts" : ["link"],
-	  "id" : "parent"
+	  "onclick" : getClickHandler()
 });
-
-chrome.contextMenus.create({"title": "Radio 1", "type": "radio", "parentId": "parent", "contexts" : ["link"], "id": "radio1",
-	"onclick": onClickHandler()});
-chrome.contextMenus.create({"title": "Radio 2", "type": "radio", "parentId": "parent", "contexts" : ["link"], "id": "radio2",
-	"onclick": onClickHandler()});
-chrome.contextMenus.create({"title": "Radio 3", "type": "radio", "parentId": "parent", "contexts" : ["link"], "id": "radio3",
-	"onclick": onClickHandler()});
